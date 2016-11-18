9039678726af8c1859f0f246c9404fabdbf69dcd
bdalvandi <bdalvandi@LD020185>
2016-11-17 21:24:35 -0800

    Add project files.

---
+++ TargetHubApi/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TargetHubApi.Models;
+
+namespace TargetHubApi.Infrastructure
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext() : base("cnnStr") { }
+
+        public DbSet<Test> Tests { get; set; }
+    }
+}
---
+++ TargetHubApi/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Owin;
+
+namespace TargetHubApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            app.UseWebApi(config);
+        }
+    }
+}
